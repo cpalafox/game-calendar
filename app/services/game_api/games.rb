10c5e4ec2a302c64597c3eaf031bde492873bba0
Cesar Palafox <cpaguila1@gmail.com>
2017-04-25 18:43:16 -0500

    Fixes fields sent, expected response from service

--- app/services/game_api/games.rb
+++ app/services/game_api/games.rb
@@ -1,14 +1,14 @@
 class GameApi::Games < GameApi
 
   def search(options)
-    Game.parse_many get("#{url}/games", default_params.merge(options))
+    Game.parse_many get("#{url}/games/", default_params.merge(options))
   end
 
   private
 
   def default_params
     super.merge({
-      fields: 'name,expected_release_day,expected_release_month,expected_release_year,expected_release_quarter,original_release_date,platforms,publishers,image,deck'
+      fields: 'name,summary,first_release_date,release_dates,cover'
     })
   end
 
--- spec/services/game_api/games_spec.rb
+++ spec/services/game_api/games_spec.rb
@@ -8,19 +8,20 @@ describe GameApi::Games do
     it 'calls api with right parameters' do
       allow(api).to receive(:get) { [] }
       allow(Game).to receive(:parse_many) { [] }
-      params = {:fields=>"name,expected_release_day,expected_release_month,expected_release_year,expected_release_quarter,original_release_date,platforms,publishers,image,deck", :limit=>10, :offset=>0, search: 'metroid'}
-      url = api.url + '/games'
+      params = {:fields=>"name,summary,first_release_date,release_dates,cover", :limit=>10, :offset=>0, search: 'mario'}
+      url = api.url + '/games/'
       expect(api).to receive(:get).with(url, params) { [] }
-      api.search(search: 'metroid')
+      api.search(search: 'mario')
     end
 
     context "with results" do
       before(:each) do
-        allow(api).to receive(:get) { [{"deck"=>"A co-operative handheld spin-off of the Metroid Prime trilogy has up to four players, as the power-suited Marines of the Galactic Federation, assisting Samus Aran in investigating a possible resurgence of Space Pirates.", "expected_release_day"=>nil, "expected_release_month"=>nil, "expected_release_quarter"=>nil, "expected_release_year"=>nil, "image"=>{ "medium_url"=>"http://www.giantbomb.com/api/image/scale_medium/2831870-screen%20shot%202016-03-03%20at%206.09.11%20pm.png" }, "name"=>"Metroid Prime: Federation Force", "original_release_date"=>"2016-08-19 00:00:00", "platforms"=>[{"api_detail_url"=>"http://www.giantbomb.com/api/platform/3045-117/", "id"=>117, "name"=>"Nintendo 3DS", "site_detail_url"=>"http://www.giantbomb.com/nintendo-3ds/3045-117/", "abbreviation"=>"3DS"}, {"api_detail_url"=>"http://www.giantbomb.com/api/platform/3045-138/", "id"=>138, "name"=>"Nintendo 3DS eShop", "site_detail_url"=>"http://www.giantbomb.com/nintendo-3ds-eshop/3045-138/", "abbreviation"=>"3DSE"}], "resource_type"=>"game"}] }
+        allow(api).to receive(:get) { {"id"=>26758, "name"=>"Super Mario Odyssey", "first_release_date"=>1514678400000, "release_dates"=>[{"category"=>6, "platform"=>130, "date"=>1514678400000, "region"=>8, "human"=>"2017-Q4", "y"=>2017, "m"=>12}], "cover"=>{"url"=>"//images.igdb.com/igdb/image/upload/t_thumb/kjfbrxvlqgvkcv4uwvif.png", "cloudinary_id"=>"kjfbrxvlqgvkcv4uwvif", "width"=>1200, "height"=>1121}} }
+        allow(Game).to receive(:parse_many) { [Game.new] }
       end
 
       it 'returns a Game array' do
-        results = api.search(search: "metroid")
+        results = api.search(search: "mario")
         expect(results).to be_kind_of Array
         expect(results.first).to be_kind_of Game
       end
@@ -29,7 +30,7 @@ describe GameApi::Games do
     context "without results" do
       it 'returns an empty array ' do
         allow(api).to receive(:get) { [] }
-        results = api.search(search: "metroid")
+        results = api.search(search: "mario")
         expect(results).to be_kind_of Array
         expect(results).to be_empty
       end
